cfeb2026434cba1f089b0ee3ab3e03c963c64c0d
crnicavic <cavic5000@tutanota.com>
2024-05-14 14:34:58 -0700

    detecting license plate without binarize is good

--- detect_plate.m
+++ detect_plate.m
@@ -1,42 +1,36 @@
 clear, clc
-%load image and convert to uint8 
-imrgb = imread('images/Cars12.png');
+
+imrgb = imread('images/Cars11.png');
+if strcmp(class(imrgb), 'double')
+    imrgb = im2double(imrgb);
+end
 %resize image
 imrgb = imresize(imrgb, 2);
+imrgb = im_eq(imrgb);
 
-%best way to equalize rgb image
-imhsv = rgb2hsv(imrgb);
-% equalize histogram for v
-v1 = imhsv(:, :, 3);
-v1 = histeq(v1);
-imhsv(:, :, 3) = v1;
-%convert back to rgb
-imrgb = hsv2rgb(imhsv);
-
-imrgb = histeq(imrgb);
 subplot(221), imshow(imrgb);
 
 %convert to gray
 imgray = rgb2gray(imrgb);
-
-%convert to black and white
-imbw = imbinarize(imgray, 0.4);
-subplot(222), imshow(imbw);
+%imgray_transformed = gray_stretch(imgray, 130/255, 1);
+%subplot(222), imshow(imgray_transformed);
 
 %now do processing to figure out what works
 %find edges between color changes
-im = edge(imbw, 'sobel');
-subplot(223), imshow(im);
+im = edge(imgray, 'sobel');
+subplot(222), imshow(im);
 
 im = imdilate(im, strel('diamond', 2));
 im = imfill(im, 'holes');
-%im = imclearborder(im);
-im = imerode(im, strel('diamond', 4'));
+subplot(223), imshow(im);
+%MORE IDEAS: Erode the shit out of the picture than just dilate it back
+im = imerode(im, strel('diamond', 10));
+im = imerode(im, strel('diamond', 10));
+im = imdilate(im, strel('square', 20));
+im = im .* imgray;
 subplot(224), imshow(im);
 imarea = numel(im);
 
-%AN IDEA!
-% look for a bounding box with the most boxes inside it
 iprops = regionprops(im, 'BoundingBox', 'Area', 'Image');
 for i = 1:length(iprops)
     %if iprops(i).Area > imarea * 0.0005 && iprops(i).Area < imarea * 0.005
@@ -44,4 +38,35 @@ for i = 1:length(iprops)
     
     subplot(221), rectangle('Position', bbox, 'EdgeColor', 'r', 'LineWidth', 2); 
     %end
+end
+
+function newim = gray_stretch(im, fmin, fmax)
+    [rows, cols] = size(im);
+    newim = zeros(rows, cols);
+    range = fmax - fmin;
+    for row = 1:rows
+        for col = 1:cols
+            if im(row, col) < fmin
+                newim(row, col) = 0;
+            elseif im(row, col) > fmax
+                newim(row, col) = 1;
+            else
+                newim(row, col) = (im(row, col) - fmin) / range;
+            end
+        end
+    end
+end
+
+%histogram eq of rgb picture
+function new_imrgb = im_eq(imrgb)
+   
+    %best way to equalize rgb image
+    imhsv = rgb2hsv(imrgb);
+    % equalize histogram for v
+    v1 = imhsv(:, :, 3);
+    v1 = histeq(v1);
+    imhsv(:, :, 3) = v1;
+    %convert back to rgb
+    new_imrgb = hsv2rgb(imhsv);
+ 
 end
